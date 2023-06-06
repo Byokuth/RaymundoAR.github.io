e0f87070da037bc640ca8b872107bc9b9c9a16d9
Byokuth <97254341+Byokuth@users.noreply.github.com>
2023-06-06 14:54:28 -0700

    a

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const marker = document.querySelector('a-marker');
 const modelo = document.getElementById("modelo");
+const modelo2 = document.getElementById("modelo2");
 
 let animControl = false;
 let textureControl = false;
@@ -17,28 +18,35 @@ marker.addEventListener('markerLost', () =>
 function controlAnimacion()
 {
     modelo.removeAttribute('animation-mixer');
+    modelo2.removeAttribute('animation-mixer');
     if(animControl)
     {
         modelo.setAttribute('animation-mixer','clip: Static Pose');
+        modelo2.setAttribute('animation-mixer','clip: Static Pose');
         animControl = false;
     }
     else
     {
         modelo.setAttribute('animation-mixer','clip: Take 001');
+        modelo2.setAttribute('animation-mixer','clip: Take 001');
         animControl = true;
     }
 }
 
 function controlTextura()
 {
+    modelo.removeAttribute('visible');
+    modelo2.removeAttribute('visible');
     if(textureControl)
     {
-        modelo.setAttribute('gltf-model','model\skipper.glb');
+        modelo.setAttribute('visible','true');
+        modelo2.setAttribute('visible','false');
         textureControl = false;
     }
     else
     {
-        modelo.setAttribute('gltf-model','model\skipper.glb');
+        modelo.setAttribute('visible','false');
+        modelo2.setAttribute('visible','true');
         textureControl = true;
     }
 }
