de446f07063b00d049c2d11a346a69d3df23a01b
Byokuth <97254341+Byokuth@users.noreply.github.com>
2023-06-06 11:52:08 -0700

    cambio de textura

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 const marker = document.querySelector('a-marker');
-const botonAnim = document.getElementById("botonAnim");
 const modelo = document.getElementById("modelo");
 
+const botonAnim = document.getElementById("botonAnim");
+const botonTexture = document.getElementById("botonTexture");
+
 let animControl = false;
+let textureControl = false;
 
 marker.addEventListener('markerFound', () => 
 {
     botonAnim.hidden = false;
+    botonTexture.hidden = false;
 });
 
 marker.addEventListener('markerLost', () => 
 {
     botonAnim.hidden = true;
+    botonTexture.hidden = true;
 });
 
 function controlAnimacion()
@@ -25,6 +30,20 @@ function controlAnimacion()
     else
     {
         modelo.setAttribute('animation-mixer','clip: Take 001');
+        animControl = true;
+    }
+}
+function controlTextura()
+{
+    modelo.removeAttribute('gltf-model');
+    if(textureControl)
+    {
+        modelo.setAttribute('gltf-model','model\skipper.glb');
+        animControl = false;
+    }
+    else
+    {
+        modelo.setAttribute('gltf-model','model\skipper2.glb');
         animControl = true;
     }
 }
